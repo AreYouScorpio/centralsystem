17723b9a4f09cbe12fa92bbd3418255644168ce9
AreYouScorpio <akos.schweger@gmail.com>
2024-01-15 21:38:21 +0100

    Initial commit

---
+++ src/main/java/hu/webuni/spring/centralsystem/CentralsystemApplication.java
@@ -0,0 +1,13 @@
+package hu.webuni.spring.centralsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/webuni/spring/centralsystem/CentralsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webuni.spring.centralsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
