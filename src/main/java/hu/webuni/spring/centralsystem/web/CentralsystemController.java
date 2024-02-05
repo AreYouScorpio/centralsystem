f2420f56631609659c89eb857b19abbf2854fe2d
AreYouScorpio <akos.schweger@gmail.com>
2024-02-05 22:21:27 +0100

    final version, no get call?

--- src/main/java/hu/webuni/spring/centralsystem/web/CentralsystemController.java
+++ src/main/java/hu/webuni/spring/centralsystem/web/CentralsystemController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @RequiredArgsConstructor
@@ -19,7 +20,7 @@ public class CentralsystemController {
 
         Random random = new Random();
 
-        System.out.println("getFreeSemesterByCentralId within CentralSystem called");
+        System.out.println("getFreeSemesterByCentralId within CentralSystem called at " + LocalDateTime.now());
 
 
         try {
